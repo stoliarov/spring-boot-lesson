1599bc0ce18e46a8ab2ca96d73ca93cc92ad4d5f
Ilya Stolyarov <i.stoliarov@hh.ru>
2023-12-26 01:01:43 +0400

    wip

--- src/main/java/ru/hh/spring_boot_lesson/config/ConditionalAutoConfiguration.java
+++ src/main/java/ru/hh/spring_boot_lesson/config/ConditionalAutoConfiguration.java
@@ -5,17 +5,19 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import ru.hh.spring_boot_lesson.service.MailService;
 
+/*
+    Это не какая-то осознанная комбинация аннотаций @Conditional... и @AutoConfigure...
+    Все они здесь приведены для примера.
+ */
 @Configuration
 //@ConditionalOnBean(MailService.class)
 @ConditionalOnBean(name = "dataSource")
@@ -26,7 +28,6 @@ import ru.hh.spring_boot_lesson.service.MailService;
     value = "some.property.example",
     havingValue = "foo"
 )
-@ConditionalOnExpression("${notification.enabled:false} && ${notification.email.enabled:false}")
 @Conditional(CustomCondition.class)
 @AutoConfigureBefore(WebMvcAutoConfiguration.class)
 @AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
